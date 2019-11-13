117cbdf6b1c7f92254d3f078439d2cd9a31021ea
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-11-12 23:40:45 -0300

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+with open(join(dirname(__file__), 'events-protocol-core/VERSION')) as _file:
+    version = _file.read().decode('ascii').strip()
+
+
+setup(
+    name='events-protocol-core',
+    version=version,
+    description='Core library for client and server libs',
+    license='Apache-2.0',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache-2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
