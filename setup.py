54e329a728239c82ff55225fd130b2c457a56846
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-11-16 21:40:50 -0300

    Updates required Python version to 3.7 instead 3.6

--- setup.py
+++ setup.py
@@ -16,9 +16,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache-2.0 License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
