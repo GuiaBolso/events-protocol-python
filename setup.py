ba272451aa73dda4dddec473ace69ab4c7d60b31
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-13 20:01:42 -0300

    Fix 'setup.py'

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from os.path import dirname, join
 from setuptools import setup, find_packages
 
 
-with open(join(dirname(__file__), 'events-protocol/VERSION')) as _file:
-    version = _file.read().decode('ascii').strip()
+with open(join(dirname(__file__), "events_protocol/VERSION")) as _file:
+    version = _file.read().strip()
 
 
 setup(
-    name='events-protocol',
+    name="events-protocol",
     version=version,
-    description='Library to be a Client and Server using event protocol',
-    license='Apache-2.0',
+    description="Library to be a Client and Server using event protocol",
+    license="Apache-2.0",
     packages=find_packages(),
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache-2.0 License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache-2.0 License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
