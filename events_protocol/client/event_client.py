249159cf23537777a908c0d44df164503562e64a
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-16 22:52:19 -0300

    Changes 'EventClient' class

--- events_protocol/client/event_client.py
+++ events_protocol/client/event_client.py
@@ -1,48 +1,32 @@
 from typing import Optional
 
 from .http import HttpClient
-from .model.response import Response
-from .exception.request_exception import (
-    TimeoutException,
-    FailedDependencyException,
-    BadProtocolException,
-)
+from .exception.request_exception import BadProtocolException
 from ..core.model.event import RequestEvent, ResponseEvent, RawEvent
-from ..core.validation.strict_event_validator import StrictEventValidator
-from ..core.exception.event_exception import EventValidationException
+from ..core.validation import EventValidator
 
 
 class EventClient:
     def __init__(self):
         self.http_client = HttpClient()
-        self.event_validator = StrictEventValidator()
+        self.event_validator = EventValidator()
         self.default_timeout = 60000
 
     def send_event(
         self, url: str, request_event: RequestEvent, timeout: Optional[int] = None
-    ) -> Response:
-        try:
-            response = self.http_client.post(
-                url=url,
-                headers={"Content-Type": "application/json", "charset": "UTF-8",},
-                payload=request_event.to_json(),
-                timeout=timeout or self.default_timeout,
-            )
-            event = self.parse_event(response)
-        except TimeoutException as e:
-            pass
-        except FailedDependencyException as e:
-            pass
-        except BadProtocolException as e:
-            pass
-        else:
-            if event.is_success:
-                pass
-            return response
+    ) -> ResponseEvent:
+        response = self.http_client.post(
+            url=url,
+            headers={"Content-Type": "application/json", "charset": "UTF-8",},
+            payload=request_event.to_json(),
+            timeout=timeout or self.default_timeout,
+        )
+        return self.parse_event(response)
 
     def parse_event(self, raw_response: str) -> ResponseEvent:
         try:
             raw_event = RawEvent.from_json(raw_response)
+            # TODO: Verify if this bellow line is necessary
             return self.event_validator.validate_as_response_event(raw_event)
-        except EventValidationException as e:
-            raise BadProtocolException(e.message)
+        except KeyError as e:
+            raise BadProtocolException("Error on parsing event response")
