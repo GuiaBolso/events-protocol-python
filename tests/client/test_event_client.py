a4984b6e2267e41656504d4f7b5d79846e460de8
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-18 23:10:58 -0300

    Modifies tests and format them with black

--- tests/client/test_event_client.py
+++ tests/client/test_event_client.py
@@ -11,80 +11,51 @@ from events_protocol.client.exception.request_exception import BadProtocolExcept
 class EventClientTest(TestCase):
     def setUp(self):
         self.client = EventClient()
-    
-    @patch('events_protocol.client.event_client.HttpClient.post')
+
+    @patch("events_protocol.client.event_client.HttpClient.post")
     def test_success_response(self, post_method):
-        event = RequestEvent(
-            name='event:name',
-            version=1,
-            id=uuid4(),
-            flow_id=uuid4(),
-            payload={'test': 'test'},
-            identity={},
-            auth={},
-            metadata={},
-        )
+        event = {
+            "name": "event:name",
+            "version": 1,
+            "payload": {"test": "test"},
+        }
         response_event = ResponseEvent(
-            name="event:name:response",
-            version=1,
-            id=uuid4(),
-            flow_id=uuid4(),
-            payload={'test': 'test'},
-            identity={},
-            auth={},
-            metadata={},
+            name="event:name:response", version=1, payload={"test": "test"},
         )
 
         post_method.return_value = response_event.to_json()
-        response = self.client.send_event('url', event)
+        response = self.client.send_event("url", **event)
 
         self.assertTrue(response.is_success)
         self.assertEqual(response, response_event)
-    
-    @patch('events_protocol.client.event_client.HttpClient.post')
+
+    @patch("events_protocol.client.event_client.HttpClient.post")
     def test_error_response(self, post_method):
-        event = RequestEvent(
-            name='event:name',
-            version=1,
-            id=uuid4(),
-            flow_id=uuid4(),
-            payload={'test': 'test'},
-            identity={},
-            auth={},
-            metadata={},
-        )
+        event = {
+            "name": "event:name",
+            "version": 1,
+            "payload": {"test": "test"},
+        }
         response_event = ResponseEvent(
-            name="event:name:error",
-            version=1,
-            id=uuid4(),
-            flow_id=uuid4(),
-            payload={'test': 'test'},
-            identity={},
-            auth={},
-            metadata={},
+            name="event:name:error", version=1, payload={"test": "test"},
         )
 
         post_method.return_value = response_event.to_json()
-        response = self.client.send_event('url', event)
-        
+        response = self.client.send_event("url", **event)
+
         self.assertTrue(response.is_error)
         self.assertEqual(EventErrorType.GENERIC, response.error_type)
         self.assertEqual(response, response_event)
-    
-    @patch('events_protocol.client.event_client.HttpClient.post')
-    def test_invalid_response(self, post_method):
-        event = RequestEvent(
-            name='event:name',
-            version=1,
-            id=uuid4(),
-            flow_id=uuid4(),
-            payload={'test': 'test'},
-            identity={},
-            auth={},
-            metadata={},
-        )
 
-        post_method.return_value = '{}'
-        
+    @patch("events_protocol.client.event_client.HttpClient.post")
+    def test_invalid_response(self, post_method):
+        event = {
+            "name": "event:name",
+            "version": 1,
+            "payload": {"test": "test"},
+        }
+
+        post_method.return_value = "{}"
+
         with self.assertRaises(BadProtocolException):
-            response = self.client.send_event('url', event)
+            response = self.client.send_event("url", **event)
