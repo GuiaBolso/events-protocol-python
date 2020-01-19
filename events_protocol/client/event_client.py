8e5c0b892eed13dcd614e9bb13bda09eaa164606
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-18 23:07:35 -0300

    Modifies 'EventClient' class to be more easy to send an event

--- events_protocol/client/event_client.py
+++ events_protocol/client/event_client.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from uuid import uuid4
+from typing import Optional, Dict
 
 from .http import HttpClient
 from .exception.request_exception import BadProtocolException
@@ -13,6 +14,24 @@ class EventClient:
         self.default_timeout = 60000
 
     def send_event(
+        self,
+        url: str,
+        name: str,
+        version: int,
+        payload: Optional[Dict] = {},
+        id: str = None,
+        flow_id: str = None,
+        identity: Dict = {},
+        auth: Dict = {},
+        metadata: Dict = {},
+        timeout: Optional[int] = None,
+    ) -> ResponseEvent:
+        request_event = self.build_request_event(
+            name, version, payload, id, flow_id, identity, auth, metadata
+        )
+        return self.send_request_event(url, request_event, timeout)
+
+    def send_request_event(
         self, url: str, request_event: RequestEvent, timeout: Optional[int] = None
     ) -> ResponseEvent:
         response = self.http_client.post(
@@ -30,3 +49,25 @@ class EventClient:
             return self.event_validator.validate_as_response_event(raw_event)
         except KeyError as e:
             raise BadProtocolException("Error on parsing event response")
+
+    def build_request_event(
+        self,
+        name: str,
+        version: int,
+        payload: Dict,
+        id: str = None,
+        flow_id: str = None,
+        identity: Dict = {},
+        auth: Dict = {},
+        metadata: Dict = {},
+    ):
+        return RequestEvent(
+            name=name,
+            version=version,
+            payload=payload,
+            id=id or str(uuid4()),
+            flow_id=flow_id or str(uuid4()),
+            identity=identity,
+            auth=auth,
+            metadata=metadata,
+        )
