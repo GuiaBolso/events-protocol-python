0a9131c1f2d8549568e062fde1e199933af9878d
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-23 22:19:01 -0300

    Makes variables on 'EventContext' class private and transform them in properties

--- events_protocol/core/context.py
+++ events_protocol/core/context.py
@@ -2,11 +2,19 @@ from contextvars import ContextVar
 
 
 class EventContext:
-    __slots__ = ["id", "flow_id"]
+    __slots__ = ["__id", "__flow_id"]
 
     def __init__(self, id, flow_id):
-        self.id = id
-        self.flow_id = flow_id
+        self.__id = id
+        self.__flow_id = flow_id
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def flow_id(self):
+        return self.__flow_id
 
 
 _context: ContextVar[EventContext] = ContextVar("event_context", default=None)
