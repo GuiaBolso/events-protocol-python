f2191c056526d8ef63236a2bc533c6e81f0755be
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-01-23 22:21:51 -0300

    Writes tests to validate 'build_event' method

--- tests/client/test_event_client.py
+++ tests/client/test_event_client.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 
 from events_protocol.core.model.event import RequestEvent, ResponseEvent
 from events_protocol.core.model.event_error_type import EventErrorType
+from events_protocol.core.context import _context, EventContext
 from events_protocol.client.event_client import EventClient
 from events_protocol.client.exception.request_exception import BadProtocolException
 
@@ -59,3 +60,46 @@ class EventClientTest(TestCase):
 
         with self.assertRaises(BadProtocolException):
             response = self.client.send_event(**event)
+
+
+class BuildEventTest(TestCase):
+    def setUp(self):
+        self.client = EventClient("http://test.com/events/")
+
+    def test_build_event_with_context(self):
+        context = EventContext("my-id", "my-flow-id")
+        _context.set(context)
+
+        event = {
+            "name": "event:name",
+            "version": 1,
+            "payload": {"test": "test"},
+            "id": "id-test",
+            "flow_id": "flow-id-test",
+        }
+        builded_event = self.client.build_request_event(**event)
+
+        self.assertEqual(context.id, builded_event.id)
+        self.assertEqual(context.flow_id, builded_event.flow_id)
+
+        _context.set(None)
+
+    def test_build_event_without_context(self):
+        event = {
+            "name": "event:name",
+            "version": 1,
+            "payload": {"test": "test"},
+            "id": "test-id",
+            "flow_id": "test-flow-id",
+        }
+        builded_event = self.client.build_request_event(**event)
+
+        self.assertEqual(event["id"], builded_event.id)
+        self.assertEqual(event["flow_id"], builded_event.flow_id)
+
+    def test_build_event_without_pass_id_and_flow_id(self):
+        event = {"name": "event:name", "version": 1, "payload": {"test": "test"}}
+        builded_event = self.client.build_request_event(**event)
+
+        self.assertIsNotNone(builded_event.id)
+        self.assertIsNotNone(builded_event.flow_id)
