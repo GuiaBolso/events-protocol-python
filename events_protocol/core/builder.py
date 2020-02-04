f335bfab3e96174143034ab2975a9c0f15ab71c8
Romulo Rosa <romulo.rosa@guiabolso.com.br>
2020-02-04 14:00:06 -0300

    DAD-330 Implement events builder

---
+++ events_protocol/core/builder.py
@@ -0,0 +1,49 @@
+import typing
+from uuid import uuid4
+
+from events_protocol.core.exception import EventException, EventNotFoundException
+from events_protocol.core.logging import JsonLogger
+from events_protocol.core.model.event import (
+    Event,
+    EventMessage,
+    PayloadType,
+    ResponseEvent,
+)
+from events_protocol.core.model.event_type import EventErrorType, EventSuccessType
+from events_protocol.core.logging.mixins.loggable import LoggableMixin
+
+
+class EventBuilder(LoggableMixin):
+    @classmethod
+    def error_for(
+        cls,
+        exc: typing.Union[EventException],
+        event: typing.Optional[Event] = Event(name="", version=1, id=str(uuid4())),
+        id_flow=str(uuid4()),
+        loggable=True,
+    ) -> ResponseEvent:
+        event_name = (
+            f"{event.name}:{exc.event_error_type}" if event.name else f"{exc.event_error_type}"
+        )
+
+        event_message = EventMessage(code=exc.code, parameters=exc.parameters).to_dict()
+        response_event = ResponseEvent.from_object(event)
+        response_event.name = event_name
+        response_event.payload = event_message
+        if loggable:
+            cls.logger.error("Event finished with error", extra=response_event.json())
+        return response_event
+
+    @staticmethod
+    def response_for(
+        cls,
+        event: Event,
+        payload: PayloadType,
+        event_type: EventSuccessType = EventSuccessType.SUCCESS,
+        loggable=True,
+    ) -> ResponseEvent:
+        response_event = ResponseEvent.from_object(event)
+        response_event.name = f"{response_event.name}:{event_type}"
+        response_event.payload = payload
+        cls.logger.info("Event finisjed with success", extra=response_event.json())
+        return response_event
