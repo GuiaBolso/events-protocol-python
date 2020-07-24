cf4a389674107680baabc9055cfa1883a8ce0b86
Jairo Santos <jairo.santos@guiabolso.com.br>
2020-07-24 10:48:26 -0300

    add common used classes to root module

--- events_protocol/__init__.py
+++ events_protocol/__init__.py
@@ -1,1 +1,9 @@
-__version__ = "0.1.3"  # pragma: no cover
+__version__ = "0.1.4"  # pragma: no cover
+
+from core.builder import EventBuilder
+from client import EventClient
+from core.model.base import BaseModel
+from core.model.event import Event, ResponseEvent
+from server.handler.event_handler import EventHandler
+from server.handler.event_handler_registry import EventRegister
+from server.parser.event_processor import EventProcessor
