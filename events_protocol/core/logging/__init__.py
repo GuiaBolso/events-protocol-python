6e2f02f9fbb637f9095e18726a8574eff75c2de5
André Ramos <andre.ramos@AGM-609829.local>
2022-05-03 14:00:19 -0300

    Deleted old method

--- events_protocol/core/logging/__init__.py
+++ events_protocol/core/logging/__init__.py
@@ -67,19 +67,6 @@ class JsonLogger(logging.LoggerAdapter):
                     ApplicationVersion=self.version,
                            ),
             )
-            #_msg = dict(
-            #    severity=logging.getLevelName(level),
-            #    logmessage=msg,
-            #    EventID=event_context.id,
-            #    FlowID=event_context.flow_id,
-            #    UserId=event_context.user_id,
-            #    UserType=event_context.user_type,
-            #    Operation="{}:v{}".format(event_context.event_name, event_context.event_version),
-            #    logger=self.klass,
-            #    LoggerName=self.logger.name,
-            #    logdate=dt.utcnow().isoformat(),
-            #    ApplicationVersion=self.version,
-            #)
 
             extra = kwargs.pop("extra", None)
             if extra:
