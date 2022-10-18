7f7ad81622e9400a5c32fa3f7d31ed3678538c8c
GBgustavodutra <gustavo.dutra@guiabolso.com>
2022-10-18 20:41:49 -0300

    Apply linter to tests

--- tests/server/handler/test_event_handler_registry.py
+++ tests/server/handler/test_event_handler_registry.py
@@ -7,23 +7,22 @@ from events_protocol.server.handler.event_handler_discovery import EventDiscover
 @dataclass
 class _TestEventHandler(EventHandler):
 
-    async def handle():
+    @classmethod
+    async def handle(cls):
         pass
 
 
-class TestEventRegister(TestCase):    
-    event_name = 'test:event'
+class TestEventRegister(TestCase):
+    event_name = "test:event"
     event_version = 1
+
     def test_event_instantiation(self):
         event_tester = _TestEventHandler(
-            event_name = self.event_name,
-            event_version = self.event_version,
+            event_name=self.event_name, event_version=self.event_version,
         )
         event_response = EventDiscovery.get(
-            event_name = event_tester.event_name, 
-            event_version = event_tester.event_version,
+            event_name=event_tester.event_name, event_version=event_tester.event_version,
         )
         self.assertIsInstance(event_response, EventHandler)
         self.assertEqual(event_response.event_name, self.event_name)
         self.assertEqual(event_response.event_version, self.event_version)
-        
